550ae095d5d4fbdea6ae9a86c7debbf5bccdb12b
badshab <badshab454@gmail.com>
2024-12-28 00:16:11 +0600

    grade f

--- python.py
+++ python.py
@@ -1,5 +1,17 @@
 #grade calculator
-mark = 90
+mark = 32
 
-if mark >= 80:
+if (mark >= 80):
+    print("Grade: A+")
+elif (mark >= 70 and mark <= 79 ):
     print("Grade: A")
+elif (mark >= 60 and mark <= 69):
+    print("Grade: B")
+elif (mark >= 50 and mark <= 59):
+    print("Grade: C")
+elif (mark >= 40 and mark <= 49):
+    print("Grade: D")
+elif (mark >= 33 and mark <= 39):
+    print("Grade: E")
+else:
+    print("Grade: F")
